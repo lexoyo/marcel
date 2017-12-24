f35d75354ab49e08297e7abb03762b4c8841fe80
Alex Hoyau <a.hoyau@silexlabs.org>
2017-12-25 00:57:34 +0100

    fix needs to .toString buffers

--- app/ear.js
+++ app/ear.js
@@ -31,7 +31,14 @@ Ear.prototype.doListen = function(lang, transitions, resolve, reject) {
   //   console.log('stdout  END ');
   // });
   cmd.stderr.on('data', data => {
-    console.error('\x1b[2mpython error: ' + data.split('\n').filter(str => str.trim() != '').join('\n\x1b[2mpython error: '));
+    try {
+      const err = data.split('\n').filter(str => str.trim() != '').join('\n\x1b[2mpython error: ');
+      console.error('\x1b[2mpython error: ', err);
+    }
+    catch(e) {
+      // more severe python error
+      console.error('\x1b[2mpython error: ', data.toString());
+    }
     //reject(data.toString());
   });
   cmd.on('exit', code => {
@@ -45,7 +52,7 @@ Ear.prototype.doListen = function(lang, transitions, resolve, reject) {
 
   // say it out loud
   cmd.stdout.on('data', output => {
-    const data = output.split('\n').join(' ').trim().toLowerCase();
+    const data = output.toString().split('\n').join(' ').trim().toLowerCase();
     console.log('\x1b[1mEar heard: ', data);
     if(['go_speak', 'wait_speak'].indexOf(data) >= 0) {
       //process.stdout.write('\x07');
@@ -64,17 +71,17 @@ Ear.prototype.beep = function() {
   var fs = require('fs');
   var wav = require('wav');
   var Speaker = require('speaker');
-   
+
   var file = fs.createReadStream('beep.wav');
   var reader = new wav.Reader();
-   
-  // the "format" event gets emitted at the end of the WAVE header 
+
+  // the "format" event gets emitted at the end of the WAVE header
   reader.on('format', function (format) {
-   
-    // the WAVE header is stripped from the output of the reader 
+
+    // the WAVE header is stripped from the output of the reader
     reader.pipe(new Speaker(format));
   });
-   
-  // pipe the WAVE file to the Reader instance 
+
+  // pipe the WAVE file to the Reader instance
   file.pipe(reader);
 };
